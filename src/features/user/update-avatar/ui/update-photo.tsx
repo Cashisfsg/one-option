b49fe042099049949b72e25dd26e0216ba197ade
Cashisfsg <cashisfsg@gmail.com>
2024-12-22 15:07:50 +0100

    update user photo

--- src/features/user/update-avatar/ui/update-photo.tsx
+++ src/features/user/update-avatar/ui/update-photo.tsx
@@ -1,18 +1,15 @@
+import { toast } from "sonner";
+import { cnBase } from "tailwind-variants";
+
 import { useUpdateUserPhotoMutation } from "@/entities/user/api";
 import { FetchUserData } from "@/entities/user/ui/fetch";
-import { cnBase } from "tailwind-variants";
+
+import { handleErrorResponse } from "@/shared/lib/helpers/handle-error-response";
+
+import IconsSprite from "@/assets/img/svg/icons-spite.svg";
 
 interface UpdateUserPhotoFormProps
     extends React.ComponentPropsWithoutRef<"label"> {}
-
-// function readFileAsBase64(file) {
-//     return new Promise((resolve, reject) => {
-//         const reader = new FileReader();
-//         reader.onload = () => resolve(reader?.result.split(",")[1]);
-//         reader.onerror = error => reject(error);
-//         reader.readAsDataURL(file);
-//     });
-// }
 
 export const UpdateUserPhotoForm: React.FC<UpdateUserPhotoFormProps> = ({
     className,
@@ -32,7 +29,7 @@ export const UpdateUserPhotoForm: React.FC<UpdateUserPhotoFormProps> = ({
         try {
             await updateUserPhoto(img).unwrap();
         } catch (error) {
-            console.error(error);
+            handleErrorResponse(error, message => toast.error(message));
         }
     };
 
@@ -40,15 +37,39 @@ export const UpdateUserPhotoForm: React.FC<UpdateUserPhotoFormProps> = ({
         <FetchUserData
             renderSuccess={({ photo }) => (
                 <label
-                    className={cnBase("cursor-pointer", className)}
+                    className={cnBase(
+                        "size-48 cursor-pointer place-items-center gap-y-1.5 overflow-clip rounded-full border-4 border-violet-primary bg-[#100033]",
+                        className
+                    )}
                     {...props}
                 >
-                    <img
-                        src={`${import.meta.env.VITE_BASE_API_URL}${photo}`}
-                        // alt="User photo"
-                        className="size-48 rounded-full border-4 border-violet-primary bg-secondary object-cover"
-                    />
-                    <span className="sr-only">Update user avatar</span>
+                    {photo ? (
+                        <>
+                            <img
+                                src={`${import.meta.env.VITE_BASE_API_URL}${photo}`}
+                                alt="Фото профиля"
+                                className="size-full object-cover"
+                            />
+                            <span className="sr-only">
+                                Обновить фото профиля
+                            </span>
+                        </>
+                    ) : (
+                        <>
+                            <svg
+                                height="3.5rem"
+                                width="3.5rem"
+                                aria-hidden="true"
+                                focusable="false"
+                                className="self-end"
+                            >
+                                <use xlinkHref={`${IconsSprite}#img-out-box`} />
+                            </svg>
+                            <span className="self-start font-secondary">
+                                Загрузить фото
+                            </span>
+                        </>
+                    )}
                     <input
                         type="file"
                         name="photo"
